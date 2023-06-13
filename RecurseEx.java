1bbb634d9e6e99b564927e6dcbe3622bbf21f73b
joesid <joesidious@gmail.com>
2023-06-13 20:31:21 +0100

    Begin Recurse

--- RecurseEx.java
+++ RecurseEx.java
@@ -1,8 +1,20 @@
 public class RecurseEx {
 
     public static void printPermutn(int[] arr, int index){
+          
+     if (index == arr.length - 1){
 
-        
+        for(int num: arr) {
+            System.out.print(num);
+        }
+        System.out.println();
+     }else {
+        for (int i = index; i < arr.length; i++){
+
+            
+        }
+     }
+
     }
     
 }
